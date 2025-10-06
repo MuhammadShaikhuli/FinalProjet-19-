dcef442caf0c2c5fd8df2d6e3f11d9339322a8f8
MuhammadShaikhuli <muhammadshaikhuli@gmail.com>
2025-10-06 15:59:36 +0700

    Tambahan 2

--- Test.java
+++ Test.java
@@ -2,5 +2,5 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hai, ini baris kode pertama saya");
         System.out.println("Halo, teman-taman. ini Project tugas MATKUL DASPRO");
-    /*Halo, Teman-Teman*/
+    /*Halo, Teman-Teman*/ /*halo*/
     }}
