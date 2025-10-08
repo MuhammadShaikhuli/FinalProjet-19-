64ac7cf9837a370c35ff3e25a0abd68c2da7b7e6
akmalfitrahari-sib <akmal.fitrahari@gmail.com>
2025-10-08 08:00:12 +0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hai, ini baris kode pertama saya");
         System.out.println("Halo, teman-taman. ini Project tugas MATKUL DASPRO");
+        System.out.println("Halo, aku akmal rekan projectmu");
     /*Halo, Teman-Teman*/ /*halo*/
     }}
